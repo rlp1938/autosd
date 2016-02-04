5432ed74bd304aa53650e7b0d5f91f1e4756173b
bob <rlp1938@gmail.com>
2016-02-05 01:28:48 +0700

    Refactored considerably to get units smaller and simpler.

--- autosd.c
+++ autosd.c
@@ -30,8 +30,8 @@
 #include "getoptions.h"
 
 typedef struct cfgdata {
-	char cfgname[128];
-	char cfgval[32];
+	char cfgname[NAME_MAX];
+	char cfgval[NAME_MAX];
 } cfgdata;
 
 typedef struct cfgprm {
@@ -40,7 +40,7 @@ typedef struct cfgprm {
 	int interval;	// when monitoring check interval minutes.
 } cfgprm;
 
-static cfgdata split_cfg_line(const char *cfgline);
+static cfgdata split_cfg_line(char *cfgline);
 static int inlist(const char *candidate, char **list);
 static void freelist(char **list);
 static void suicide(void);
@@ -49,6 +49,10 @@ static void check_prior_instance_running(char *progname);
 static cfgprm get_config_parameters(const char *relpath);
 static void sanity_check(int what, int lt, int gt, const char *thename);
 static void check_power_status(int monitor, cfgprm prms);
+static void check_set_config_values(int res, cfgprm *prms, cfgdata cd);
+static void get_cfg_name(char *src, char *name, const char sep);
+static void get_cfg_value(char *src, char *val, const char sep);
+static void strip_space(char *buf);
 
 int main(int argc, char **argv)
 {
@@ -61,28 +65,11 @@ int main(int argc, char **argv)
 	return 0;
 }//main()
 
-cfgdata split_cfg_line(const char *cfgline)
+cfgdata split_cfg_line(char *cfgline)
 {
 	cfgdata retval;
-	memset (retval.cfgname, 0, 128);
-	memset (retval.cfgval, 0, 32);
-	char buf[NAME_MAX];
-	strcpy(buf, cfgline);
-	char *equals = strchr(buf, '=');
-	if (!equals) {
-		fprintf(stderr, "Corrupt config line: %s\n", cfgline);
-		exit(EXIT_FAILURE);
-	}
-	*equals = '\0';
-	strcpy(retval.cfgname, buf);
-	equals++;
-	while(isspace(*equals)) equals++;	// get past leading ' '
-	strcpy(retval.cfgval, equals);
-	// remove trailing ' '
-	char *cp = &retval.cfgname[strlen(retval.cfgname)-1];
-	while(isspace(*cp)) cp--;
-	cp = &retval.cfgval[strlen(retval.cfgval)-1];
-	while(isspace(*cp)) cp--;
+	get_cfg_name(cfgline, retval.cfgname, '=');
+	get_cfg_value(cfgline, retval.cfgval, '=');
 	return retval;
 } // split_cfg_line()
 
@@ -109,12 +96,18 @@ void freelist(char **list)
 } // freelist()
 
 void suicide(void)
-{	/*
-	* Shut myself down - and yes I need to be running as root to do it.
-	*/
-	char *command = "shutdown -h now";
-	printf("fake shutdown.\n");
-}
+{	/* Shut myself down - must be root. */
+	char *command = "dbus-send --system "
+	"--dest=org.freedesktop.ConsoleKit --type=method_call --print-reply"
+	" --reply-timeout=2000 /org/freedesktop/ConsoleKit/Manager "
+	"org.freedesktop.ConsoleKit.Manager.Stop";
+	int res = system(command);
+	if (res == -1) {
+		fprintf(stderr, "Command failed: %s\n", command);
+		exit(EXIT_FAILURE);
+	}
+
+} // suicide()
 
 void is_this_first_run(char *progname)
 {
@@ -147,27 +140,7 @@ static cfgprm get_config_parameters(const char *relpath)
 							,(char *)NULL };
 		cfgdata cd = split_cfg_line(cflines[cflidx]);
 		int res = inlist(cd.cfgname, list);
-		switch (res)
-		{
-			case 0:
-				prms.interval = strtol(cd.cfgval, NULL, 10);
-				sanity_check(prms.interval, 1, 8*60, "check_interval");
-				prms.interval *= 60; // given in min., but I want secs.
-				break;
-			case 1:
-				prms.batmon = strtol(cd.cfgval, NULL, 10);
-				sanity_check(prms.batmon, 10, 100, "monitor_level");
-				break;
-			case 2:
-				prms.batquit = strtol(cd.cfgval, NULL, 10);
-				sanity_check(prms.batquit, 1, 100, "quit_level");
-				break;
-			default:
-				fprintf(stderr, "Unknown parameter name in config file:"
-						" %s\n", cd.cfgname);
-				exit(EXIT_FAILURE);
-				break;
-		} // switch(res)
+		check_set_config_values(res, &prms, cd);
 		cflidx++;
 	} // while()
 	freelist(cflines);
@@ -206,3 +179,88 @@ static void check_power_status(int monitor, cfgprm prms)
 		poweroff = (sysbuf[0] == '0');
 	} // while(poweroff)
 } // check_power_status()
+
+void check_set_config_values(int res, cfgprm *prms, cfgdata cd)
+{
+	switch (res)
+	{
+		case 0:
+			prms->interval = strtol(cd.cfgval, NULL, 10);
+			sanity_check(prms->interval, 1, 8*60, "check_interval");
+			prms->interval *= 60; // given in min., but I want secs.
+			break;
+		case 1:
+			prms->batmon = strtol(cd.cfgval, NULL, 10);
+			sanity_check(prms->batmon, 10, 100, "monitor_level");
+			break;
+		case 2:
+			prms->batquit = strtol(cd.cfgval, NULL, 10);
+			sanity_check(prms->batquit, 1, 100, "quit_level");
+			break;
+		default:
+			fprintf(stderr, "Unknown parameter name in config file:"
+					" %s\n", cd.cfgname);
+			exit(EXIT_FAILURE);
+			break;
+	} // switch(res)
+} // check_set_config_values()
+
+void get_cfg_name(char *src, char *name, const char sep)
+{
+	char buf[NAME_MAX];
+	char *begin = src;
+	char *end = strchr(src, sep);
+	size_t nlen;
+	if (!end) {
+		fprintf(stderr, "No valid separator in 'get_cfg_name()': %c\n",
+				sep);
+		exit(EXIT_FAILURE);
+	}
+	nlen = end - begin;
+	if (nlen > NAME_MAX - 1) {
+		fprintf(stderr, "String too large in 'get_cfg_name()': %lu\n",
+				nlen);
+		exit(EXIT_FAILURE);
+	}
+	strncpy(buf, begin, nlen);
+	buf[nlen] = '\0';
+	strip_space(buf);
+	strcpy(name, buf);
+} // get_cfg_name()
+
+void get_cfg_value(char *src, char *val, const char sep)
+{
+	char buf[NAME_MAX];
+	char *begin = strchr(src, sep);
+	if (!begin) {
+		fprintf(stderr, "No valid separator in 'get_cfg_value()': %c\n",
+				sep);
+		exit(EXIT_FAILURE);
+	}
+	begin++;	// get past sep
+	char *end = begin + strlen(src) - 1;
+	size_t nlen;
+	nlen = end - begin;
+	if (nlen > NAME_MAX - 1) {
+		fprintf(stderr, "String too large in 'get_cfg_value()': %lu\n",
+				nlen);
+		exit(EXIT_FAILURE);
+	}
+	strncpy(buf, begin, nlen);
+	buf[nlen] = '\0';
+	strip_space(buf);
+	strcpy(val, buf);
+} // get_cfg_value()
+
+static void strip_space(char *buf)
+{
+	char *target = strdup(buf);
+	char *begin = target;
+	while (isspace(*begin)) begin++;
+	char *end = target + strlen(target) - 1;
+	while (isspace(*end)) end--;
+	size_t len = end - begin + 1;
+	strncpy(buf, begin, len);
+	buf[len] = '\0';
+	free(target);
+} // strip_space()
